a99c7a0cd2c1d8576999f9c91eb8b3a734e3eb83
FelipeSoft <felipedecastrolima2@gmail.com>
2023-03-20 10:12:20 -0300

    Estilização da página Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,27 +17,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Início</title>
+    <link rel="stylesheet" href="http://localhost/controle-de-estoque-laravel/public/assets/css/style.css">
+    <script src="https://use.fontawesome.com/a443ad607e.js"></script>
 </head>
 <body>
     <header>
-        <nav>
-            <div class="nav-f">
-                <ul>
-                    <li><a href="">Dashboard</a></li>
-                    <li><a href="">Transações</a></li>
-                    <li><a href="">Cadastro</a></li>
-                    <li><a href="">Configurações</a></li>
-                </ul>
-            </div>
-            <div class="nav-s">
-                <div class="search">
-
+        <nav class="top-nav">
+            <div class="max-width">
+                <div class="nav-f">
+                    <ul>
+                        <li class="active"><a href="">Dashboard</a></li>
+                        <li><a href="">Transações</a></li>
+                        <li><a href="">Cadastro</a></li>
+                        <li><a href="">Configurações</a></li>
+                    </ul>
                 </div>
-                <div class="profile">
-                    
+                <div class="nav-s">
+                    <div class="search">
+                        <form action="" method="post">
+                            <button><i class="fa fa-search"></i></button>
+                            <input type="search" name="" id="" placeholder="Pesquisar...">
+                        </form>
+                    </div>
+                    <div class="profile">
+                        <img src="http://localhost/controle-de-estoque-laravel/public/assets/img/profile.jpg" alt="Aqui está a foto do seu perfil">
+                    </div>
                 </div>
             </div>
         </nav>
+        <nav class="middle-nav">
+            <div class="max-width">
+                <h2>Seja bem-vindo novamente, Felipe !</h2>
+                <nav class="middle-nav-2">
+                    <ul>
+                        <li class="active-2"><a href="">Geral</a></li>
+                        <li><a href="">Transações</a></li>
+                        <li><a href="">Estoque</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
     </header>
+
+    <section id="dashboard-overview">
+        <div class="max-width">
+            <div class="stock-value">
+                <p class="dashboard-title">Valor do Estoque</p>
+                <h1>R$ 1.098.489,97</h1>
+                <button class="dashboard-button"><i class="fa fa-plus" style="padding-right: 5px;"></i>Compra</button>
+                <button class="dashboard-button"><i class="fa fa-plus" style="padding-right: 5px;"></i>Venda</button>
+            </div>
+            <div class="plans">b</div>
+
+            <div class="general">c</div>
+            <div class="transactions-historical">d</div>
+        </div>
+    </section>
 </body>
 </html>
